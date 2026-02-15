a374744eaf417777d563e988114b5e92b8406404
marcoalfoonso <marcoalfoonso.rodcal@gmail.com>
2026-02-15 23:51:13 +0000

    se agregó el mensaje de "pagina lista"

---
+++ programaJava.js
@@ -0,0 +1,19 @@
+//se crea una constante log que tendrá como parámetro msg
+const log = msg => {
+    //el, contendrá el elemento que esté en log del html
+    const el = document.getElementById('log');
+    // se creará un parrafo p en el html
+    const p = document.createElement('p');
+    //el parrafo p, contendrá el parámetro msg
+    p.textContent = msg;
+    //se agragrá dinámicamente p dentro del div del html y por debajo del p anterior
+    el.appendChild(p);
+};
+
+//se asegurará de que se cargue la página correctamente antes de que se ejecute alguna acción
+document.addEventListener('DOMContentLoaded',()=>{
+    const el = document.getElementById('log');
+    el.textContent = "Página lista";
+});
+
+
