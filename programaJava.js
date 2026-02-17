faa876575095eddfbe50064459c6a875f0f3ea4a
marcoalfoonso <marcoalfoonso.rodcal@gmail.com>
2026-02-17 16:54:21 +0000

    se agregaron dos botones nuevos para apagar y prender el led desde mqtt

--- programaJava.js
+++ programaJava.js
@@ -1,3 +1,21 @@
+//conectar a un brocker mqtt
+const client = mqtt.connect("9c5b62f9383143779c1c998df33d398c.s1.eu.hivemq.cloud:8883/mqtt",{
+    clientId: "web_" + Math.random().toString(16).slice(2, 10),
+    username: "marcoAlfoonso",
+    password: "Lic72@S2",
+    clean: true
+});
+
+//muestra en la consola que la conexión fue correcta
+client.on("connect", () => {
+  console.log("Conectado con autenticación");
+});
+
+//muestra el error en la consola
+client.on("error", (err) => {
+  console.error("Error:", err);
+});
+
 //se crea una constante log que tendrá como parámetro msg
 const log = msg => {
     //el, contendrá el elemento que esté en log del html
@@ -12,10 +30,25 @@ const log = msg => {
 
 //se asegurará de que se cargue la página correctamente antes de que se ejecute alguna acción
 document.addEventListener('DOMContentLoaded',()=>{
+    //se identifica el elemento botón del html
     const el = document.getElementById('btn-saludo');
+    //cuando suceda el evento 'click' se llamará a log
     el.addEventListener('click',()=>{
        const al = document.getElementById('log');
-       al.textContent = "Botón presionado" ;
+       al.textContent = "Hola." ;
+    });
+
+    //se identifica el botón que prende
+    const ledon = document.getElementById('btn-ledon');
+    //cuando suceda el evento click del botón led
+    ledon.addEventListener('click',()=>{
+        client.publish("casa/luz","ON");
+    });
+
+    const ledoff = document.getElementById('btn-ledoff');
+
+    ledoff.addEventListener('click',()=>{
+        client.publish("casa/luz","OFF");
     });
     
 });
